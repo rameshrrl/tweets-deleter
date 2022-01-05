e6f0a4eef10457fe66249b104f9e5d2301da6f60
ramesh <lrsramesh13@gmail.com>
2022-01-05 18:41:20 +0530

    Refactored the oauth inputs

--- controllers/deleteTweets.controller.js
+++ controllers/deleteTweets.controller.js
@@ -1,6 +1,7 @@
 import { makeRequest } from '../helpers/makeRequest';
 import { getUserTweets } from './getUserTweets.controller';
 import { generateNonce } from '../helpers/generateNonce';
+import { generateSignature } from '../helpers/generateSignature';
 
 export const deleteTweets = async() => {
     const response = await getUserTweets();
@@ -8,18 +9,25 @@ export const deleteTweets = async() => {
     const tweets = response?.data;
     const meta = response?.meta;
 
+    const HTTPMethod = 'DELETE';
+    let tweetID = '';
+    const url = `https://api.twitter.com/2/tweets/${tweetID}`;
     const nonce = generateNonce();
     const timestamp = new Date().getTime();
-    
-    const Authorization = `OAuth oauth_consumer_key="${process.env.API_KEY}",oauth_nonce="${nonce}",oauth_signature="Sa6XalKvgmcRb4KDOme9edSlVnk%3D",oauth_signature_method="HMAC-SHA1",oauth_timestamp="${timestamp}",oauth_token="${process.env.ACCESS_TOKEN}",oauth_version="1.0"`;
+    const oAuthConsumerKey = process.env.API_KEY;
+    const oAuthToken = process.env.ACCESS_TOKEN;
+    const oAuthSignatureMethod = 'HMAC-SHA1';
+    const oAuthVersion = '1.0';
 
-    const tweetID = '122112111';
+    const signature = await generateSignature({HTTPMethod, url, nonce, timestamp, oAuthConsumerKey, oAuthToken, oAuthSignatureMethod, oAuthVersion});
+    
+    const Authorization = `OAuth oauth_consumer_key="${oAuthConsumerKey}",oauth_nonce="${nonce}",oauth_signature="${signature}",oauth_signature_method="${oAuthSignatureMethod}",oauth_timestamp="${timestamp}",oauth_token="${oAuthToken}",oauth_version="${oAuthVersion}"`;
 
     const options =  {
-        method: 'DELETE',
+        method: HTTPMethod,
         headers: {'Authorization': Authorization }
     }
 
-    const deleted = await makeRequest(`https://api.twitter.com/2/tweets/${tweetID}`, options);
+    const deleted = await makeRequest(url, options);
 
 }
---
+++ helpers/generateSignature.js
@@ -0,0 +1,3 @@
+export const generateSignature = async (oAuthCredentials) => {
+
+}

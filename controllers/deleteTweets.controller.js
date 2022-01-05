8e22618c8f4566ca721b33873eb110ca49d5095c
ramesh <lrsramesh13@gmail.com>
2022-01-05 20:25:00 +0530

    Finished the sinle tweet delete flow

--- controllers/deleteTweets.controller.js
+++ controllers/deleteTweets.controller.js
@@ -13,15 +13,17 @@ export const deleteTweets = async() => {
     let tweetID = '';
     const url = `https://api.twitter.com/2/tweets/${tweetID}`;
     const nonce = generateNonce();
-    const timestamp = new Date().getTime();
-    const oAuthConsumerKey = process.env.API_KEY;
+    const timestamp = Math.round(Date.now() / 1000);
+    const oAuthConsumer = process.env.API_KEY;
     const oAuthToken = process.env.ACCESS_TOKEN;
+    const oAuthConsumerSecret = process.env.API_KEY_SECRET;
+    const oAuthTokenSecret = process.env.ACCESS_TOKEN_SECRET;
     const oAuthSignatureMethod = 'HMAC-SHA1';
     const oAuthVersion = '1.0';
 
-    const signature = await generateSignature({HTTPMethod, url, nonce, timestamp, oAuthConsumerKey, oAuthToken, oAuthSignatureMethod, oAuthVersion});
-    
-    const Authorization = `OAuth oauth_consumer_key="${oAuthConsumerKey}",oauth_nonce="${nonce}",oauth_signature="${signature}",oauth_signature_method="${oAuthSignatureMethod}",oauth_timestamp="${timestamp}",oauth_token="${oAuthToken}",oauth_version="${oAuthVersion}"`;
+    const signature = await generateSignature({HTTPMethod, url, nonce, timestamp, oAuthConsumer, oAuthToken, oAuthSignatureMethod, oAuthVersion, oAuthConsumerSecret, oAuthTokenSecret});
+
+    const Authorization = `OAuth oauth_consumer_key="${oAuthConsumer}",oauth_nonce="${nonce}",oauth_signature="${signature}",oauth_signature_method="${oAuthSignatureMethod}",oauth_timestamp="${timestamp}",oauth_token="${oAuthToken}",oauth_version="${oAuthVersion}"`;
 
     const options =  {
         method: HTTPMethod,
--- helpers/generateSignature.js
+++ helpers/generateSignature.js
@@ -1,3 +1,61 @@
+import jsSHA from 'jssha';
+
 export const generateSignature = async (oAuthCredentials) => {
 
+    let baseString = oAuthBaseString(oAuthCredentials.HTTPMethod, oAuthCredentials.url, oAuthCredentials.oAuthConsumer, oAuthCredentials.oAuthToken, oAuthCredentials.timestamp, oAuthCredentials.nonce);
+    let signingKey = oAuthSigningKey(oAuthCredentials.oAuthConsumerSecret, oAuthCredentials.oAuthTokenSecret);
+    let signature = oAuthSignature(baseString, signingKey);
+
+    return signature;
+
 }
+
+function oAuthBaseString(method, url, key, token, timestamp, nonce) {
+    return method
+        + '&' + percentEncode(url)
+        + '&' + percentEncode(genSortedParamStr(key, token, timestamp, nonce));
+};
+
+const oAuthSigningKey = (consumer_secret, token_secret) => {
+    return consumer_secret + '&' + token_secret;
+};
+
+const oAuthSignature = (base_string, signing_key) => {
+    var signature = hmac_sha1(base_string, signing_key);
+    return percentEncode(signature)
+};
+
+const percentEncode = (str) => {
+    return encodeURIComponent(str).replace(/[!*()']/g, (character) => {
+        return '%' + character.charCodeAt(0).toString(16);
+    });
+};
+
+const hmac_sha1 = (string, secret) => {
+    let shaObj = new jsSHA("SHA-1", "TEXT");
+    shaObj.setHMACKey(secret, "TEXT");
+    shaObj.update(string);
+    let hmac = shaObj.getHMAC("B64");
+    return hmac;
+};
+
+const genSortedParamStr = (key, token, timestamp, nonce) => {
+    let paramObj = {
+        oauth_consumer_key: key,
+        oauth_nonce: nonce,
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_timestamp: timestamp,
+        oauth_token: token,
+        oauth_version: '1.0'
+    }
+    let paramObjKeys = Object.keys(paramObj);
+    let len = paramObjKeys.length;
+    paramObjKeys.sort();
+    let paramStr = paramObjKeys[0] + '=' + paramObj[paramObjKeys[0]];
+    for (var i = 1; i < len; i++) {
+        paramStr += '&' + paramObjKeys[i] + '=' + percentEncode(decodeURIComponent(paramObj[paramObjKeys[i]]));
+    }
+    return paramStr;
+};
+
+// Copied the code from this article -  https://imagineer.in/blog/authorizing-twitter-api-calls-in-javascript/

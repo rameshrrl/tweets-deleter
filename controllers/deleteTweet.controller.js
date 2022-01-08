850244ef4882dd2ac847c126d15c53dc4f0bd737
ramesh <lrsramesh13@gmail.com>
2022-01-08 13:19:38 +0530

    All API's authenticated with OAuth

--- controllers/deleteTweet.controller.js
+++ controllers/deleteTweet.controller.js
@@ -1,17 +1,15 @@
 import { makeRequest } from '../helpers/makeRequest';
 import { generateOAuthHeader } from '../helpers/generateOAuthHeader';
+import { generateHTTPOptions } from '../helpers/generateHTTPOptions';
 
 export const deleteTweet = async (tweetID) => {
 
     const HTTPMethod = 'DELETE';
     const url = `https://api.twitter.com/2/tweets/${tweetID}`;
 
-    const Authorization = await generateOAuthHeader(HTTPMethod, url)
+    const authorization = await generateOAuthHeader(HTTPMethod, url);
 
-    const options =  {
-        method: HTTPMethod,
-        headers: {'Authorization': Authorization }
-    }
+    const options = generateHTTPOptions(HTTPMethod, authorization);
 
     const deleted = await makeRequest(url, options);
 

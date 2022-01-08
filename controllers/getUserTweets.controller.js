3b80a147d2cf7adfa158bbf1156a98c55e37b343
ramesh <lrsramesh13@gmail.com>
2022-01-08 16:17:06 +0530

    Added ability to fetch before given days

--- controllers/getUserTweets.controller.js
+++ controllers/getUserTweets.controller.js
@@ -2,6 +2,7 @@ import { makeRequest } from '../helpers/makeRequest';
 import { getUserId } from './getUserId.controller';
 import { generateOAuthHeader } from '../helpers/generateOAuthHeader';
 import { generateHTTPOptions } from '../helpers/generateHTTPOptions';
+import { givenDaysAgo } from '../helpers/givenDaysAgo';
 
 export const getUserTweets = async () => {
 
@@ -11,17 +12,21 @@ export const getUserTweets = async () => {
         if(!userId) throw new Error('User not found!');
     
         const HTTPMethod = 'GET';
-        const url = `https://api.twitter.com/2/users/${userId}/tweets`;
+        let url = `https://api.twitter.com/2/users/${userId}/tweets`;
+
+        const deleteBefore = parseInt(process.env.DELETE_BEFORE) || 7;
+        const day = givenDaysAgo(deleteBefore);
     
         const reqParams = {
-            max_results: 100
+            max_results: 100,
+            end_time: day
         }
     
         const authorization = await generateOAuthHeader(HTTPMethod, url, reqParams);
 
         const options = generateHTTPOptions(HTTPMethod, authorization);
     
-        const response = await makeRequest(`${url}?max_results=100`, options);
+        const response = await makeRequest(`${url}?max_results=100&end_time=${day}`, options);
     
         if(response?.data?.length) {
             return response?.data;

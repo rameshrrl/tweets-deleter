fedabe31390d9ff857b1d953bc71d88daaed710c
Ramesh L <lrsramesh13@gmail.com>
2022-01-16 15:04:19 +0530

    Separated baseURL into separate file

--- controllers/deleteTweet.controller.js
+++ controllers/deleteTweet.controller.js
@@ -1,11 +1,12 @@
 import { makeRequest } from '../helpers/makeRequest';
 import { generateOAuthHeader } from '../helpers/generateOAuthHeader';
 import { generateHTTPOptions } from '../helpers/generateHTTPOptions';
+import { TwitterV2BaseURL } from '../helpers/twitterBaseURL';
 
 export const deleteTweet = async (tweetID) => {
 
     const HTTPMethod = 'DELETE';
-    const url = `https://api.twitter.com/2/tweets/${tweetID}`;
+    const url = `${TwitterV2BaseURL}/tweets/${tweetID}`;
 
     const authorization = await generateOAuthHeader(HTTPMethod, url);
 
--- controllers/getUserId.controller.js
+++ controllers/getUserId.controller.js
@@ -1,12 +1,13 @@
 import { makeRequest } from '../helpers/makeRequest';
 import { generateOAuthHeader } from '../helpers/generateOAuthHeader';
 import { generateHTTPOptions } from '../helpers/generateHTTPOptions';
+import { TwitterV2BaseURL } from '../helpers/twitterBaseURL';
 
 export const getUserId = async () => {
     
     const username = process.env.TWITTER_USER_NAME;
     const HTTPMethod = 'GET';
-    const url = `https://api.twitter.com/2/users/by/username/${username}`;
+    const url = `${TwitterV2BaseURL}/users/by/username/${username}`;
 
     const authorization = await generateOAuthHeader(HTTPMethod, url);
 
